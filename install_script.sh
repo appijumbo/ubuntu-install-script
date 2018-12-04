79f4cb41bda2bc013409dd82ab434557247a4c33
Tom Ormiston <tom@appijumbo.com>
2018-12-04 11:29:06 +0000

    ammended etcher appimage install

--- install_script.sh
+++ install_script.sh
@@ -361,7 +361,7 @@ install_etcher () {
     etcher_version="etcher-electron-1.4.6-linux-x64.zip"
     etcher_url="https://github.com/balena-io/etcher/releases/download/v1.4.6/"
     
-    wget -O /opt/appimages/etcher $etcher_url/$etcher_version
+    wget -O /opt/appimages/$etcher_version $etcher_url/$etcher_version
     sudo chmod 775 /opt/appimages/$etcher_version
     sudo unzip /opt/appimages/$etcher_version
     #sudo rm /opt/appimages/etcher.zip
